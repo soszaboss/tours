ab3ff4e98eb7eabb8caa03d339c4efcbef561c45
soszaboss <moustoifa10@outlook.fr>
2024-01-27 14:26:00 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React, { useState, useEffect } from 'react'
 import Loading from './Loading'
 import Tours from './Tours'
+import data from './data';
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
-function App() {
-  return <h2>Tours Project Setup</h2>
-}
 
+const url = 'https://course-api.com/react-tours-project';
+
+function App() {
+  const [loading, setLoading] = useState(true)
+  const [tours, setTour] = useState([])
+
+  // async function fetchData() {
+  //   try {
+  //     const response = await fetch(url);
+  //     const data = await response.json(); // This line is added
+  //     setLoading(false); // This line is modified
+  //     setTour(data);
+  //   } catch (error) {
+  //     throw new Error(error);
+  //   }
+  // }
+
+  // useEffect(() => {fetchData()}, [])
+  
+  function tourData(data) {
+          setLoading(false);
+          setTour(data)
+  }
+  useEffect(() => {tourData(data)}, [])
+  return (
+            <main>
+              {
+                loading ? (<Loading />) : (<Tours tours={tours} />)
+              }
+              
+            </main>
+  )
+}
 export default App
--- src/Tour.js
+++ src/Tour.js
@@ -1,7 +1,25 @@
 import React, { useState } from 'react';
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({id, image, name, price, info}) => {
+  const [readMore, setReadMore] = useState(false)
+  return (
+    <article>
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className='tour-price'>{price}</h4>
+        </div>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button onClick={()=>{setReadMore(!readMore)}}> {readMore ? 'Show Less' : 'Read More'} </button>
+        </p>
+        <button className='delete-btn'>
+          Not Intersted
+        </button>
+      </footer>
+    </article> 
+  );
 };
 
 export default Tour;
